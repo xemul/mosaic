1d835fd3ba1b8ed20c915bce75f954b004f7bd56
Pavel Emelyanov <xemul@parallels.com>
2015-05-12 18:39:36 +0400

    test: Prepare aging test for thin
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- test/ageing.sh
+++ test/ageing.sh
@@ -3,90 +3,105 @@
 set -x
 
 . env.sh
-t_location="tess.loc"
-m_dir="mnt"
-tess_type="overlay"
+
+m_dir="mnt/"
+
+clean_ts() {
+	echo "No tesserae storage"
+}
 
 clean() {
 	rm -f "mosaic.conf"
 	umount "mosaic.status"
 	rmdir "mosaic.status"
 	rmdir "$m_dir"
-	rm -rf "$t_location"
+	clean_ts
 }
 
 clean
 
-touch "mosaic.conf"
-mkdir "mosaic.status"
-mkdir "$t_location"
-mkdir "$m_dir"
+run_tests() {
+	touch "mosaic.conf"
+	mkdir "mosaic.status"
+	mkdir "$m_dir"
+	prep_ts
 
-echo "* Check tessera ageing"
-$moctl "tessera" "add" "t.a" $tess_type "$t_location/a" \
-		|| fail "T-Add a"
-$moctl "tessera" "mount" "t.a" $m_dir \
-		|| fail "T-Mount a"
-echo "V0" > "$m_dir/file-a" \
-		|| fail "Touch a"
-$moctl "tessera" "umount" "t.a" \
-		|| fail "T-Umount a"
+	echo "* Check tessera ageing"
+	t_args=$(new_ts_args "a")
+	$moctl "tessera" "add" "t.a" $tess_type $t_args \
+			|| fail "T-Add a"
 
-$moctl "tessera" "grow" "t.a" "1" \
-		|| fail "T-Grow 1"
+	$moctl "tessera" "mount" "t.a" $m_dir \
+			|| fail "T-Mount a"
+	echo "V0" > "$m_dir/file-a" \
+			|| fail "Touch a"
+	$moctl "tessera" "umount" "t.a" \
+			|| fail "T-Umount a"
 
-$moctl "tessera" "mount" "t.a:1" $m_dir \
-		|| fail "T-Mount a:1"
-fgrep "V0" "$m_dir/file-a" \
-		|| fail "Old data in grow"
-echo "V1" > "$m_dir/file-a" \
-		|| fail "Update t-data"
+	$moctl "tessera" "grow" "t.a" "1" \
+			|| fail "T-Grow 1"
 
-$moctl "tessera" "show" "t.a" > "t.a.show"
-"$yamlck" "t.a.show" || fail "T-Show ages mounted in yaml"
-cat "t.a.show" | fgrep "age: 1" || fail "T-Show age 1"
-cat "t.a.show" | fgrep -A1 "mounted" | fgrep "$m_dir" || fail "T-Show mounted age 1"
-rm -f "t.a.show"
+	$moctl "tessera" "mount" "t.a:1" $m_dir \
+			|| fail "T-Mount a:1"
+	fgrep "V0" "$m_dir/file-a" \
+			|| fail "Old data in grow"
+	echo "V1" > "$m_dir/file-a" \
+			|| fail "Update t-data"
 
-$moctl "tessera" "umount" "t.a:1" \
-		|| fail "Umount a:1"
+	$moctl "tessera" "show" "t.a" > "t.a.show"
+	"$yamlck" "t.a.show" || fail "T-Show ages mounted in yaml"
+	cat "t.a.show" | fgrep "age: 1" || fail "T-Show age 1"
+	cat "t.a.show" | fgrep -A1 "mounted" | fgrep "$m_dir" || fail "T-Show mounted age 1"
+	rm -f "t.a.show"
 
-$moctl "tessera" "mount" "t.a" $m_dir \
-		|| fail "T-Mount a (2)"
-fgrep "V0" "$m_dir/file-a" \
-		|| fail "Old data in base"
-$moctl "tessera" "umount" "t.a" \
-		|| fail "T-Umount a (2)"
+	$moctl "tessera" "umount" "t.a:1" \
+			|| fail "Umount a:1"
 
-echo "* Test mosaic construction out of aged tesserae"
+	$moctl "tessera" "mount" "t.a" $m_dir "ro" \
+			|| fail "T-Mount a (2)"
+	fgrep "V0" "$m_dir/file-a" \
+			|| fail "Old data in base"
+	$moctl "tessera" "umount" "t.a" \
+			|| fail "T-Umount a (2)"
 
-$moctl "mosaic" "add" "m0" "t.a:0:t" \
-		|| fail "M-Add 0"
+	echo "* Test mosaic construction out of aged tesserae"
 
-$moctl "mosaic" "add" "m1" "t.a:1:t" \
-		|| fail "M-Add 1"
+	$moctl "mosaic" "add" "m0" "t.a:0:t:options=ro" \
+			|| fail "M-Add 0"
 
-mkdir "$m_dir/t"
+	$moctl "mosaic" "add" "m1" "t.a:1:t" \
+			|| fail "M-Add 1"
 
-$moctl "mosaic" "mount" "m0" "$m_dir" \
-		|| fail "M-Mount 0"
+	mkdir "$m_dir/t"
 
-fgrep "V0" "$m_dir/t/file-a" \
-		|| fail "Old file in m0"
-$moctl "mosaic" "umount" "m0" \
-		|| fail "M-Umount 0"
+	$moctl "mosaic" "mount" "m0" "$m_dir" \
+			|| fail "M-Mount 0"
 
-$moctl "mosaic" "mount" "m1" "$m_dir" \
-		|| fail "M-Mount 1"
-fgrep "V1" "$m_dir/t/file-a" \
-		|| fail "New file in m0"
-$moctl "mosaic" "umount" "m1" \
-		|| fail "M-Umount 1"
+	fgrep "V0" "$m_dir/t/file-a" \
+			|| fail "Old file in m0"
+	$moctl "mosaic" "umount" "m0" \
+			|| fail "M-Umount 0"
 
-rmdir "$m_dir/t"
+	$moctl "mosaic" "mount" "m1" "$m_dir" \
+			|| fail "M-Mount 1"
+	fgrep "V1" "$m_dir/t/file-a" \
+			|| fail "New file in m0"
+	$moctl "mosaic" "umount" "m1" \
+			|| fail "M-Umount 1"
 
-$moctl "tessera" "del" "t.a" \
-		|| fail "T-Del"
+	rmdir "$m_dir/t"
+
+	$moctl "tessera" "del" "t.a" \
+			|| fail "T-Del"
+}
+
+# echo "###### Running tests for overlay"
+# . ovl.sh
+# run_tests
+
+echo "###### Running tests for thin"
+. thin.sh
+run_tests
 
 clean
 echo "All tests passed"
