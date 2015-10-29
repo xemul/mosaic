7834881936643cb5991fcf7ca3d7fc4081ec4d07
Pavel Emelyanov <xemul@parallels.com>
2015-10-29 16:31:16 +0400

    log: For DOTEST compiles just use printf-s
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/log.h
+++ include/log.h
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include "uapi/mosaic.h"
 
+#ifndef DOTEST
 extern mosaic_log_fn log_fn;
 extern enum log_level log_level;
 
@@ -13,6 +14,9 @@ extern enum log_level log_level;
 					__FILE__, __LINE__,		\
 					##__VA_ARGS__);			\
 		} while (0)
+#else
+#define print_log(level, fmt, ...) printf(fmt, ##__VA_ARGS__)
+#endif
 
 #define logd(fmt, ...)		print_log(LOG_DBG, "debug: " fmt, ##__VA_ARGS__)
 #define log(fmt, ...)		print_log(LOG_INF, fmt, ##__VA_ARGS__)
